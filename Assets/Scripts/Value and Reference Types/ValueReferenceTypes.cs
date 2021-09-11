3797c805b565ba9bef631012f7be344fee9cc71e
synith <synith@live.ca>
2021-09-11 11:46:42 -0600

    DOTS Example w/ Struct
    with Unity DOTS we created a job that increments all the values in a native array by 1.
    because this array is using a struct for value we needed to reassign the value to the array

--- Assets/Scripts/Value and Reference Types/ValueReferenceTypes.cs
+++ Assets/Scripts/Value and Reference Types/ValueReferenceTypes.cs
@@ -1,29 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Burst;
 
 public class ValueReferenceTypes : MonoBehaviour
 {
     private void Start()
     {
+        /*
         MyTestClass first = new MyTestClass(7);
         MyTestClass second = first;
         second.value = 5;
-        Debug.Log("Reference Type example: " + first.value);
+        Debug.Log("Class: " + first.value);
 
-        int a = 7;
-        int b = a;
-        b = 5;
-        Debug.Log("Value Type example: " + a);
+        MyTestStruct firstStruct = new MyTestStruct(7);
+        MyTestStruct secondStruct = firstStruct;
+        secondStruct.value = 5;
+        Debug.Log("Struct: " + firstStruct.value);
+        */
+
+        NativeArray<MyTestStruct> testNativeArray = new NativeArray<MyTestStruct>(new MyTestStruct[] {
+        new MyTestStruct(1),
+        new MyTestStruct(2),
+        new MyTestStruct(3)
+        }, Allocator.TempJob);
+        TestJob testJob = new TestJob { testNativeArray = testNativeArray };
+        testJob.Run();
+
+        for (int i = 0; i < testNativeArray.Length; i++)
+        {
+            Debug.Log(testNativeArray[i].value);
+        }
+        testNativeArray.Dispose();
     }
 }
-
 public class MyTestClass
 {
     public int value;
-
     public MyTestClass(int value)
     {
         this.value = value;
+    }
+}
+public struct MyTestStruct
+{
+    public int value;
+    public MyTestStruct(int value)
+    {
+        this.value = value;
+    }
+}
+
+public struct TestJob : IJob
+{
+    public NativeArray<MyTestStruct> testNativeArray;
+    public void Execute()
+    {
+        for (int i = 0; i < testNativeArray.Length; i++)
+        {
+            MyTestStruct myStruct = testNativeArray[i];
+            myStruct.value++;
+            testNativeArray[i] = myStruct;
+        }
     }
 }
