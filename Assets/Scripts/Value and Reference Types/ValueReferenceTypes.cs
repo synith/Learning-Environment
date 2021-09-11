17c1d45af8d6fb726d9a1f63ec7c308f3fee1bc6
synith <synith@live.ca>
2021-09-11 09:57:33 -0600

    value vs reference example
    reference example shows how both variables get changed when changing one of them since they're both holding references to the same object
    
    value type example shows how when we assign a to b we are not passing a reference to the original data but instead assigning it to a copy of the original data

--- Assets/Scripts/Value and Reference Types/ValueReferenceTypes.cs
+++ Assets/Scripts/Value and Reference Types/ValueReferenceTypes.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class ValueReferenceTypes : MonoBehaviour
 {
-    bool testBool;
-
     private void Start()
     {
-        Debug.Log(testBool);
+        MyTestClass first = new MyTestClass(7);
+        MyTestClass second = first;
+        second.value = 5;
+        Debug.Log("Reference Type example: " + first.value);
+
+        int a = 7;
+        int b = a;
+        b = 5;
+        Debug.Log("Value Type example: " + a);
+    }
+}
+
+public class MyTestClass
+{
+    public int value;
+
+    public MyTestClass(int value)
+    {
+        this.value = value;
     }
 }
