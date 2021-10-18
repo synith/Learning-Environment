d21c11532d4fbee2708bb6f28ad381cc313fd7ec
synith <synith@live.ca>
2021-10-18 15:01:37 -0600

    set up simple game client
    using learning environment to set up a scene called Simple-Game-Client which will talk to our Simple-Game-Server JavaScript project to store and request information.
    
    Using SimpleJSON parser from quill18's github repo https://github.com/quill18/GuessEmClient/blob/main/Assets/SimpleJSON.cs

---
+++ Assets/Scripts/Simple Game Client/ServerTalker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerTalker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make a web request to get info from the server
+        // this will be a text response.
+
+        // That text, is actually JSON info, so we need to
+        // parse that into something we can navigate.
+
+        // Output some stuff to the console so that we know
+        // that it worked.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
